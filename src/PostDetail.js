26eb08afd1050dd1f72ded24701b14f6695e0a58
Derek Antrican <derekantrican@gmail.com>
2023-12-30 11:16:00 -0800

    Simplify how data is passed to PostDetail components

--- src/PostDetail.js
+++ src/PostDetail.js
@@ -6,10 +6,11 @@ import remarkGfm from 'remark-gfm';
 
 function PostDetail(props) {
     //Todo: There will probably need to be some markdown rendering in the comments (or both)
+    //Todo: show images (& galleries), embeds (eg videos), etc
     return (
       <div style={{display: 'flex', flexDirection: 'column', padding: 10}}>
-        <PostDetailHeader data={props.data} close={props.close}/>
-        <PostDetailComments data={props.data}/>
+        <PostDetailHeader data={props.data[0].data.children[0].data /*The 'post data' section*/} close={props.close}/>
+        <PostDetailComments data={props.data[1].data.children /*The 'comments' section*/}/>
       </div>
     );
 }
@@ -22,16 +23,16 @@ function PostDetailHeader(props) {
         <div style={{display: 'flex', flexDirection: 'row'}}>
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <i style={{height: 15, width: 20, fontSize: '15px'}} className='bi bi-caret-up-fill'/>
-            <div>{props.data[0].data.children[0].data.score}</div>
+            <div>{props.data.score}</div>
             <i style={{height: 15, width: 20, fontSize: '15px'}} className='bi bi-caret-down-fill'/>
         </div>
-        <div style={{margin: 10, fontWeight: 'bold'}}>{props.data[0].data.children[0].data.title}</div>
+        <div style={{margin: 10, fontWeight: 'bold'}}>{props.data.title}</div>
         <div style={{flex: '1 0 0'}}/>{/*Fill available space so close button is always at the far right*/}
         <i style={{height: 30, width: 30, fontSize: '25px', marginLeft: 10}} className='bi bi-x-lg' onClick={() => props.close()}/>
         </div>
-        {props.data[0].data.children[0].data.selftext ?
+        {props.data.selftext ?
           <div style={{overflowWrap: 'anywhere'}}>
-            <Markdown remarkPlugins={[remarkGfm]}>{props.data[0].data.children[0].data.selftext}</Markdown>
+            <Markdown remarkPlugins={[remarkGfm]}>{props.data.selftext}</Markdown>
           </div>
         : null}
     </div>
@@ -41,7 +42,7 @@ function PostDetailHeader(props) {
 function PostDetailComments(props) {
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
-      {props.data[1].data.children.map(c =>
+      {props.data.map(c =>
         <div key={c.id} style={{display: 'flex', flexDirection: 'row', borderWidth: '2px 0px 0px 0px', borderStyle: 'solid', borderColor: 'gray', padding: 5}}>
           <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <i style={{height: 15, width: 20, fontSize: '15px'}} className='bi bi-caret-up-fill'/>
