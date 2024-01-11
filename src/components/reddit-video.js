ff646a8a53672ef9c28a8ec1094c2407e75d9ea6
Derek Antrican <derekantrican@gmail.com>
2024-01-10 16:10:25 -1000

    Added some todos

--- src/components/reddit-video.js
+++ src/components/reddit-video.js
@@ -1,15 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 
 function RedditVideoPlayer(props) {
-  const [audioUrl, setAudioUrl] = useState(null);
+  //Todo: support reddit video gifs
+
   //https://stackoverflow.com/a/67464583/2246411
   //This *kinda* works for play & pause, but not really at all for seek. Also, the audio isn't truly connected to the video (eg the "mute" control
   //on the video doesn't work). Maybe should find out how to create a "blob" like RES:
   //https://github.dev/honestbleeps/Reddit-Enhancement-Suite/blob/21217ded9dd12f4e998f9e67477191afa92e0c9d/lib/modules/showImages.js
   
+  const [audioUrl, setAudioUrl] = useState(null);
+  
   useEffect(() => {
     async function getAudioUrl() {
       if (props.sourceUrl) {
+        //Todo: cache results
         //Parse the audio url from the DASHPlaylist
         const baseMediaUrl = props.sourceUrl.replace(/(?<=\/)DASH.*/, '');
         const response = await fetch(baseMediaUrl + 'DASHPlaylist.mpd');
