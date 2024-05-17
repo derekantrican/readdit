5901c6d4369f2ad167d9b0b02b83dd9b7c394703
Derek Antrican <derekantrican@gmail.com>
2024-05-16 21:04:19 -0700

    First pass at gallery support

--- src/components/embeds/ImageGallery.js
+++ src/components/embeds/ImageGallery.js
@@ -1,21 +1,47 @@
+import { useEffect, useState } from "react";
+
 function ImageGallery(props) {
-  //TEMP (until we can return a proper, pageable display)
-  const getImage = () => {
-    var firstImageData;
-    for (var media of props.post.gallery_data.items) {
+  const [images, setImages] = useState([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  
+  useEffect(() => {
+    const imageList = [];
+    for (var media of props.post.gallery_data.items) { //gallery_data dictates the order of the images
       if (props.post.media_metadata[media.media_id].status == 'valid') {
-        firstImageData = props.post.media_metadata[media.media_id];
-        break;
+        var metadata = props.post.media_metadata[media.media_id];
+        imageList.push(metadata.s.u ?? metadata.s.gif);
       }
     }
 
-    if (firstImageData) {
-      return firstImageData.s.u ?? firstImageData.s.gif;
-    }
+    setImages(imageList);
+  }, []);
+
+  const buttonStyle = {
+    position: 'absolute',
+    top: '50%',
+    borderRadius: '50%',
+    borderColor: 'transparent',
+    height: 32,
+    width: 30,
+    display: 'flex',
+    justifyContent: 'center',
   }
 
   return (
-    <img height='100%' width='100%' style={{marginTop: 10}} src={getImage()}/>
+    <div style={{position: 'relative', marginTop: 10}}>
+      {/*Todo: currently this causes the image block to change size whenever the image changes (if it's a different size)*/}
+      <img height='100%' width='100%' src={images[currentImageIndex]}/>
+      {currentImageIndex > 0 ?
+        <button style={{...buttonStyle, left: 5}} onClick={() => setCurrentImageIndex(currentImageIndex - 1)}>
+          <i style={{fontSize: '25px'}} className='bi bi-arrow-left-short'/>
+        </button>
+      : null}
+      {currentImageIndex < images.length - 1 ?
+        <button style={{...buttonStyle, right: 5}} onClick={() => setCurrentImageIndex(currentImageIndex + 1)}>
+          <i style={{fontSize: '25px'}} className='bi bi-arrow-right-short'/>
+        </button>
+      : null}
+    </div>
   );
 }
 
