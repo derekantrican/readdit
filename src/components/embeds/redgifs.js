ca304f3face35ce8c6feb14c5ec4ff4276c53c31
Derek Antrican <derekantrican@gmail.com>
2024-08-20 15:55:54 -0700

    Improve redgifs iframe performance by only loading when visible

--- src/components/embeds/redgifs.js
+++ src/components/embeds/redgifs.js
@@ -1,33 +1,19 @@
-import { useEffect, useState } from "react";
-
-let token = null;
+import { useRef } from "react";
+import useOnScreen from "../../hooks/useOnScreen";
 
 function RedGifsPlayer(props) {
-  //Todo: I would prefer to get the direct link. Looks like I can do that by getting a temporary token, then calling the API: 
-  // https://github.com/yt-dlp/yt-dlp/blob/0b6f829b1dfda15d3c1d7d1fbe4ea6102c26dd24/yt_dlp/extractor/redgifs.py#L68
-
-  useEffect(() => {
-    async function getDirectLink() {
-      if (!token) {
-        const response = await fetch('https://api.redgifs.com/v2/auth/temporary');
-        const responseJson = await response.json();
-        token = responseJson.token;
-      }
-
-      //Todo: fetch the direct link
-    }
-
-    getDirectLink();
-  }, []);
-
   const parseRedGifsId = (url) => {
-    var regExp = /redgifs\.com\/watch\/(\w*)/;
+    var regExp = /redgifs\.com\/(watch|ifr)\/(\w*)/;
     var match = url.match(regExp);
-    return match && match[1];
+    return match && match[2];
   }
+
+  const ref = useRef(null);
+  const isVisible = useOnScreen(ref); //Only load iframe when visible
   
   return (
-    <iframe height={props.height} width='100%' style={{marginTop: 10}} allowFullScreen sandbox src={`https://redgifs.com/ifr/${parseRedGifsId(props.url)}`}/>
+    <iframe ref={ref} height={props.height} width='100%' style={{marginTop: 10}} allowFullScreen sandbox
+      src={isVisible ? `https://redgifs.com/ifr/${parseRedGifsId(props.url)}` : null}/>
   );
 }
 
