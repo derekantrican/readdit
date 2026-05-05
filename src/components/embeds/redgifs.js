7b8c129315c03915186722fd27dc8ab8c56601fc
derekantrican <derekantrican@gmail.com>
2026-05-05 08:34:55 -0700

    Switch back to iframe for redgifs since redgifs blocks API requests with CORS

--- src/components/embeds/redgifs.js
+++ src/components/embeds/redgifs.js
@@ -1,19 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-
-let tokenPromise = null;
-function getRedGifsToken() {
-  if (!tokenPromise) {
-    tokenPromise = fetch('https://api.redgifs.com/v2/auth/temporary')
-      .then(res => res.json())
-      .then(data => data.token)
-      .catch(err => {
-        tokenPromise = null; // Allow retry on failure
-        throw err;
-      });
-  }
-  
-  return tokenPromise;
-}
+import { useRef } from "react";
+import useOnScreen from "../../hooks/useOnScreen";
 
 function RedGifsPlayer(props) {
   const parseRedGifsId = (url) => {
@@ -23,38 +9,11 @@ function RedGifsPlayer(props) {
   }
 
   const ref = useRef(null);
-  const [videoUrl, setVideoUrl] = useState(null);
-
-  useEffect(() => {
-    async function fetchVideoUrl() {
-      const id = parseRedGifsId(props.url);
-      if (!id) 
-        return;
-
-      try {
-        const token = await getRedGifsToken();
-
-        // Fetch the gif details using the API (id must be lowercase)
-        const gifResponse = await fetch(`https://api.redgifs.com/v2/gifs/${id.toLowerCase()}`, {
-          headers: { 'Authorization': `Bearer ${token}` }
-        });
-        const gifData = await gifResponse.json();
-        setVideoUrl(gifData.gif?.urls?.hd || gifData.gif?.urls?.sd);
-      } 
-      catch (err) {
-        console.error('Failed to fetch RedGifs video URL:', err);
-      }
-    }
-
-    if (!videoUrl) {
-      fetchVideoUrl();
-    }
-  }, [props.url, videoUrl]);
-
+  const isVisible = useOnScreen(ref); //Only load iframe when visible
+  
   return (
-    <video ref={ref} height={props.height} width="100%" style={{marginTop: 10}} controls loop>
-      {videoUrl && <source src={videoUrl} type="video/mp4" />}
-    </video>
+    <iframe ref={ref} height={props.height} width='100%' style={{marginTop: 10}} allowFullScreen sandbox
+      src={isVisible ? `https://redgifs.com/ifr/${parseRedGifsId(props.url)}` : null}/>
   );
 }
 
